424187808f50647e65b88a993a86dcde8f96f45e
jmefdev <94146423+jmefdev@users.noreply.github.com>
2025-05-21 01:41:20 -0600

    mobile fixes

--- app/components/VideoPlayer.tsx
+++ app/components/VideoPlayer.tsx
@@ -51,7 +51,7 @@ export default function VideoPlayer({ videoUrl, isMuted, setIsMuted }: VideoPlay
   }
 
   return (
-    <div className="relative w-full min-h-screen border-4 border-red-500" style={{ height: '100dvh', paddingBottom: 'env(safe-area-inset-bottom)', boxSizing: 'border-box' }}>
+    <div className="relative w-full border-4 border-red-500" style={{ height: 'calc(100dvh - env(safe-area-inset-bottom))', boxSizing: 'border-box' }}>
       <div className="w-full h-full overflow-hidden rounded-none relative" ref={playerContainerRef}>
         <ReactPlayer
           url={fullVideoUrl}
