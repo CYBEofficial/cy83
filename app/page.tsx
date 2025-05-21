cc6a529305bb79094e6025ce529f499634e7eeb2
jmefdev <94146423+jmefdev@users.noreply.github.com>
2025-05-21 01:10:41 -0600

    inline video

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import VideoPlayer from './components/VideoPlayer';
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen min-h-screen min-w-full bg-black overflow-hidden">
-      <VideoPlayer 
-        videoUrl="/media/whatgoesundercomesunder.mp4"
-      />
+    <main className="min-h-screen bg-black flex flex-col items-center justify-center px-2 py-4 md:px-8 md:py-12">
+      <div className="w-full max-w-2xl md:max-w-4xl lg:max-w-6xl aspect-video">
+        <VideoPlayer 
+          videoUrl="/media/whatgoesundercomesunder.mp4"
+        />
+      </div>
     </main>
   );
 }
